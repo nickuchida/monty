3184e024be060955c9aff74dba3ea638ffc43e32
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-06 07:50:28 +0000

    betty style opcodes

--- opcodes.c
+++ opcodes.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ *push - push element to stack
+ *@stack: stack
+ *@line_number: line number
+ *Return: void
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
@@ -8,21 +14,33 @@ void push(stack_t **stack, unsigned int line_number)
 	printf("print push\n");
 }
 
+/**
+ *pall - print all
+ *@stack: the stack
+ *@line_number: line number
+ *Return: void
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
 	(void)stack;
-
-	printf("print pall\n");
-/*	while (*stack)
-	{
-		stack.n = *stack;
-		printf("%d\n", *stack->n);
-		stack = *stack->next;
-	}
+/*
+ *	printf("print pall\n");
+ *	while (*stack)
+ *	{
+ *		(*stack) = (*stack)->n;
+ *		printf("%d\n", (*stack)->n);
+ *		(*stack) = (*stack)->next;
+ *	}
 */
 }
 
+/**
+ *nop - no purpose
+ *@stack: void
+ *@line_number: void
+ *Return: void
+ */
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
